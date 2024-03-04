9f6605b6a58236ca716535bb079dacc56fd2e0fd
Andres E. Parilli <andres@theuforyou.com>
2024-03-04 19:01:03 -0400

    Se integra esqueleto de formulario

--- src/components/NavMenu/index.tsx
+++ src/components/NavMenu/index.tsx
@@ -2,34 +2,27 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { NavLink } from 'react-router-dom'; // Only import NavLink here
 
-const NavMenu=() => {
-
-    return (
-        <Navbar expand="lg" className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar> 
-
-    );
+const NavMenu = () => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="/">The U For You</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <NavLink to="/">Home</NavLink>
+            <NavDropdown title="Instituciones" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/institucion/1">Institucion 1</NavDropdown.Item>
+              <NavDropdown.Item href="/institucion/2">Institucion 2</NavDropdown.Item>
+              <NavDropdown.Item href="/institucion/3">Institucion 3</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
+
 export default NavMenu;
--- src/pages/Institucion/index.tsx
+++ src/pages/Institucion/index.tsx
@@ -1,16 +1,37 @@
-import { useParams } from 'react-router-dom';
 import NavMenu from '../../components/NavMenu';
+import { useForm } from 'react-hook-form';
 
-
+interface FormData {
+  institucion: string;
+  scan: string;
+  quality: string;
+  notes: string;
+}
 
 const Institucion = () => {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm<FormData>();
+  const onSubmit = (data: FormData) => console.log(data);
 
-return(
+  console.log(watch('institucion')); // watch input value by passing the name of it
+
+  return (
     <>
-    <NavMenu />
-    <div>SCAN</div>
+      <NavMenu />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input defaultValue="institucion" {...register('institucion')} />
+        <input {...register('scan', { required: true })} />
+        {errors.scan && <span>This field is required</span>}
+
+        <input {...register('quality', { required: true })} />
+        {errors.quality && <span>This field is required</span>}
+
+        <input {...register('notes', { required: true })} />
+        {errors.notes && <span>This field is required</span>}
+
+        <input type="submit" />
+      </form>
     </>
-);
+  );
 };
 
 export default Institucion;
